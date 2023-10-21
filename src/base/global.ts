867043b697d07089d0a485764aff9834131c4a8d
Aaron Fabian <aaronfabian78@gmail.com>
2023-10-21 13:40:23 +0900

    fixing global path

--- src/base/global.ts
+++ src/base/global.ts
@@ -14,18 +14,18 @@ export const TILE_SIZE = 64;
 
 // window . global
 const gTextures = new Map<string, HTMLImageElement>();
-gTextures.set('explosion', newImage('./../../asset/explosion.png'));
-gTextures.set('game-map', newImage('./../../asset/gameMap.png'));
-gTextures.set('projectile', newImage('./../../asset/projectile.png'));
-gTextures.set('orc', newImage('./../../asset/orc.png'));
-gTextures.set('tower', newImage('./../../asset/tower.png'));
+gTextures.set('explosion', newImage('../../asset/explosion.png'));
+gTextures.set('game-map', newImage('../../asset/gameMap.png'));
+gTextures.set('projectile', newImage('../../asset/projectile.png'));
+gTextures.set('orc', newImage('../../asset/orc.png'));
+gTextures.set('tower', newImage('../../asset/tower.png'));
 
 const gSounds = new Map<string, any>();
-gSounds.set('field-battle', newSound('./../../sounds/field_battle.wav', true));
-gSounds.set('game-over', newSound('./../../sounds/game_over.ogg'));
-gSounds.set('rock-break', newSound('./../../sounds/rock_break.ogg'));
-gSounds.set('player-get-hit', newSound('./../../sounds/player_get_hit.mp3'));
-gSounds.set('start-song', newSound('./../../sounds/start_song.mp3', true));
+gSounds.set('field-battle', newSound('../../sounds/field_battle.wav', true));
+gSounds.set('game-over', newSound('../../sounds/game_over.ogg'));
+gSounds.set('rock-break', newSound('../../sounds/rock_break.ogg'));
+gSounds.set('player-get-hit', newSound('../../sounds/player_get_hit.mp3'));
+gSounds.set('start-song', newSound('../../sounds/start_song.mp3', true));
 
 (window as any).gTextures = gTextures;
 (window as any).gSounds = gSounds;
